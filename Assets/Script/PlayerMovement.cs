7fda687713fabc4930ecb3b0db384435e02933cb
VirtualKalle <38258930+VirtualKalle@users.noreply.github.com>
2019-05-30 12:02:36 +0200

    Created movement scripts

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-Vector3.right * Time.deltaTime * speed);
+        }
+    }
+}
